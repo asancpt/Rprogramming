98dcd11462ea65332ea80b041b306d7845c0ca19
shanmdphd <pil.han@gmail.com>
2017-04-06 15:12:07 +0900

    krantz

--- README.R
+++ README.R
@@ -1,4 +1,8 @@
 knitr::knit('index.Rmd', output = 'README.md')
 Index <- readLines("README.md")
 YML <- grep("---", Index)
-writeLines(Index[-seq(YML[1], YML[2])], "README.md")
+Index <- Index[-seq(YML[1], YML[2])]
+Index[1] <- "[![DOI](https://zenodo.org/badge/86778696.svg)](https://zenodo.org/badge/latestdoi/86778696)"
+writeLines(, "README.md")
+
+
